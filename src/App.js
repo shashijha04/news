9c4fc99c5dc0af6f2e114b2f813ad4555680e193
shashi <shashi.jha04@gmail.com>
2021-02-03 11:55:58 +0530

    deploying app to github

--- src/App.js
+++ src/App.js
@@ -12,18 +12,25 @@ class App extends Component {
           <Header />
         </div>
 
-        <div className='Home'>
+        <div className='Home_row'>
           <Channel title='Aaj tak' image='https://akm-img-a-in.tosshub.com/aajtak/resource/img/bemisaal-20-saal/aajtak-logo-71X52.png' link='https://www.aajtak.in/' />
 
           <Channel title='Ndtv' image='https://drop.ndtv.com/homepage/images/ndtvlogo23march.png' link='https://www.ndtv.com/' />
 
           <Channel title='Abp Live' image='https://static.abplive.com/frontend/images/ABP_logo_main.svg' link='https://www.abplive.com/' />
 
+        </div>
+
+        <div className='Home_row'>
           <Channel title='India Today' image='https://akm-img-a-in.tosshub.com/indiatoday/../sites/all/themes/itg/logo.png' link='https://www.indiatoday.in/' />
 
           <Channel title='Zee News' image='https://english.cdn.zeenews.com/images/logo/zeenewslogo_nav.png' link='https://zeenews.india.com/' />
 
           <Channel title='RSTv' image='https://rstv.nic.in/wp-content/uploads/2016/12/rstv-logo.jpg' link='https://rstv.nic.in/' />
+
+        </div>
+
+        <div className='Home_row'>
 
           <Channel title='LokSabha Tv' image='https://loksabhatv.nic.in/sites/default/files/logo.png' link='https://loksabhatv.nic.in/' />
 
@@ -31,6 +38,9 @@ class App extends Component {
 
           <Channel title='R Bharat' image='https://bharat.republicworld.com/assets/images/header/r-bharat.png' link='https://bharat.republicworld.com/livetv' />
 
+        </div>
+
+        <div className='Home_row'>
           <Channel title='Lallantop' image='https://smedia2.intoday.in/lallantop/wp-content/uploads/2016/05/lallantop_mob.png' link='https://www.thelallantop.com/' />
 
           <Channel title='The Quint' image='' link='https://www.thequint.com/' />
--- src/Channel.js
+++ src/Channel.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Button,Card} from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
-const Channel=(props)=> {
+const Channel = (props) => {
     return (
         <div className='card'>
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={props.image} />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Button variant="primary" href={props.link} target="_blank">Click me</Button>
-        </Card.Body>
-      </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img className='image' variant="top" src={props.image} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Button variant="primary" href={props.link} target="_blank">Click to visit site</Button>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
